a450df6ba9e86c284fa1322c588009192139653f
MSPTEST <matthew.pasco@studentpartner.com>
2015-03-24 18:11:02 -0400

    Adding Content

---
+++ MSP Demo/MSP Demo/MSP Demo.Shared/Classes/MSP.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSP_Demo.Classes
+{
+    class MSP
+    {
+        private string _name;
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private string _college;
+        public string college
+        {
+            get { return _college; }
+            set { _college = value; }
+        }
+
+        public MSP(string nm, string col)
+        {
+            _name = nm;
+            _college = col;
+        }
+    }
+}
